62d27838cd307f83fafdd79333a9f9dd93571a85
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-09-19 15:59:51 +0200

    feat: add Page for shopping cart

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import ProductCard from "../components/ProductCard.jsx";
+import "../styles/CartPage.scss"
+
+export default function CartPage() {
+
+    return (
+        <>
+            <Header/>
+            
+            <Footer/>
+        </>
+    )
+}
