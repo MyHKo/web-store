0f5c2825829fcdd749244c84c798d66cc11a1b24
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-09-19 20:26:07 +0200

    feat: add functionality for cart and logo buttons on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,30 @@
 import "../styles/Header.scss"
 import logo from "../assets/logo.svg"
 import cart from "../assets/cart.svg"
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 
 export default function Header() {
+
+    const navigate = useNavigate();
+
+    function goToCart(){
+        navigate("/cart");
+    }
+
+    function goToHome(){
+        navigate("/");
+    }
+
     return (
         <header>
             <div className="headerContainer">
-                <img className="logoImage" src={logo} alt="logo"/>
+                <img className="logoImage" src={logo} alt="logo" onClick={goToHome}/>
                 <form>
                     <input className="searcher" type="text" placeholder="Search"/>
                 </form>
                 <h2><NavLink className="homeButton" to="/">Home</NavLink></h2>
                 <h2><NavLink className="catalogButton" to="/catalog">Catalog</NavLink></h2>
-                <img className="cartImage" src={cart} alt="cart"/>
+                <img className="cartImage" src={cart} alt="cart" onClick={goToCart}/>
             </div>
         </header>
     )
