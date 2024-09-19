cd795299387c9eeba3ec2937b3f1c67e4e99703a
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-09-19 13:56:31 +0200

    style: finish Home page style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Products from "../components/Products";
-import Filters from "../components/Filters";
 import "../styles/Home.scss"
 
 export default function Home() {
@@ -9,7 +8,6 @@ export default function Home() {
         <>
             <Header />
             <div className="mainContainer">
-                <Filters />
                 <Products />
             </div>
             <Footer />
