dbde4ab55ade41bef2d2df267ab820ebfd3cb46a
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-10-08 14:29:10 +0200

    struct: change import statements in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './styles/index.css'
-import store from './redux/store.js'
+import '@styles/index.css'
+import store from '@redux/store.js'
 import {Provider} from "react-redux";
 
 createRoot(document.getElementById('root')).render(
